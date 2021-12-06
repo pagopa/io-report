a54ea82a5fe57cde6546c6631eb1efb4b1da1da8
Matteo Boschi <mbmaken@gmail.com>
2021-12-06 10:34:18 +0100

    update README

--- src/models/io_users.py
+++ src/models/io_users.py
@@ -8,3 +8,13 @@ class IOUsersReport(Report):
 		# don't know how to retrieve this data programmatically
 		self.data = format_number(14010930)
 		return self.data
+
+
+class MyAwesomeReport(Report):
+
+	def load_data(self) -> any:
+		self.data = format_number(self.my_db_precious_data())
+		return self.data
+
+	def my_db_precious_data(self):
+		pass
