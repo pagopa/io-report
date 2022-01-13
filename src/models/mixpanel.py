9fd69cb476d343ad4f91af78dd59a3de569e0837
Matteo Boschi <mbmaken@gmail.com>
2022-01-13 09:50:42 +0100

    order stats items

--- src/models/mixpanel.py
+++ src/models/mixpanel.py
@@ -42,19 +42,11 @@ _mixpanel_reports = [
 	{"description": "# richieste di cancellazione del profilo", "id": 26787097},
 	{"description": "# richieste di download dei dati del profilo", "id": 26787100},
 	{"description": "% di dispositivi con autenticazione biometrica", "id": 15212227},
-	{"description": "% verifica pagamento effettuata con successo", "id": 15352169,
-	 "extractor": lambda item: item["successo"]["all"]},
-	{"description": "% attivazione pagamento effettuata con successo", "id": 15352239,
-	 "extractor": lambda item: item["successo"]["all"]},
-	{"description": "# pagamenti effettuati con successo", "id": 27163336},
-	{"description": "% pagamenti effettuati con successo", "id": 26999327,
-	 "extractor": lambda item: item["successo"]["all"]},
+
 	{"description": "% utenti che abbandonano l'inserimento di un pagamento durante il checkout", "id": 26999381,
 	 "extractor": lambda item: item["interruzione"]["all"]},
 	{"description": "% utenti che abbandonano l'inserimento di una carta di credito durante il checkout",
 	 "id": 13913973},
-	{"description": "% carte di credito aggiunte con successo", "id": 13913969},
-	{"description": "# carte di credito aggiunte nel wallet", "id": 15272532},
 	{"description": "% sceglie la preferenza AUTOMATICA per i servizi", "id": 15507584},
 	{"description": "% che condivide i dati con Mixpanel", "id": 13828137, "extractor": lambda item: (item[
 																										  "MIXPANEL_SET_ENABLED - Unique"][
@@ -64,17 +56,27 @@ _mixpanel_reports = [
 																										  "MIXPANEL_SET_ENABLED - Unique"][
 																										  "$overall"][
 																										  "all"]) * 100},
-	{"description": "# account Paypal aggiunti", "id": 27301649,
-	 "extractor": lambda item: item["successo"]["$overall"]["all"]},
-	{"description": "% successo nell'aggiunta di un account Paypal", "id": 27301649,
+	{"description": "% carte di credito aggiunte con successo", "id": 13913969},
+	{"description": "# carte di credito aggiunte nel wallet", "id": 15272532},
+	{"description": "% account Paypal aggiunti con successo", "id": 27301649,
 	 "extractor": lambda item: (item["successo"]["$overall"]["all"] / (
 			 (item["fallimento"]["$overall"]["all"] if 'fallimento' in item else 0) + item["successo"]["$overall"][
 		 "all"])) * 100},
+	{"description": "# account Paypal aggiunti nel wallet", "id": 27301649,
+	 "extractor": lambda item: item["successo"]["$overall"]["all"]},
+	{"description": "% verifica pagamento effettuata con successo", "id": 15352169,
+	 "extractor": lambda item: item["successo"]["all"]},
+	{"description": "% attivazione pagamento effettuata con successo", "id": 15352239,
+	 "extractor": lambda item: item["successo"]["all"]},
+	{"description": "# pagamenti effettuati con successo", "id": 27163336},
+	{"description": "% pagamenti effettuati con successo", "id": 26999327,
+	 "extractor": lambda item: item["successo"]["all"]},
 	{"description": "# pagamenti effettuati con carta di credito", "id": 27302180,
 	 "extractor": lambda item: item["carta di credito"].get("0", {"all": 0})[
 		 "all"] if "carta di credito" in item else 0},
 	{"description": "# pagamenti effettuati con Paypal", "id": 27302180,
 	 "extractor": lambda item: item["paypal"]["$overall"].get("0", {"all": 0})["all"] if "paypal" in item else 0},
+
 ]
 
 mixpanel_reports = []
