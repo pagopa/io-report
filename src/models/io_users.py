ea27e6f7c0ed198b7cb36a3d3684e21e344bba22
Matteo Boschi <mbmaken@gmail.com>
2022-03-23 10:41:14 +0100

    fix

--- src/models/io_users.py
+++ src/models/io_users.py
@@ -6,5 +6,5 @@ class IOUsersReport(Report):
 
 	def load_data(self) -> any:
 		# don't know how to retrieve this data programmatically, PR welcome :D
-		self.data = format_number(15591971)
+		self.data = format_number(0)
 		return self.data
